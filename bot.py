209cc986ba309ffce3d5e237179f58ca4dd91747
Marco Gomiero <marco.gomiero.93@gmail.com>
2017-12-20 17:14:04 +0100

    added picture

--- bot.py
+++ bot.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-import telepot, os
+import telepot
+import os
 
 # Config vars
 token = os.environ['TELEGRAM_TOKEN']
 
-#material
+# material
 gifBestemmia = 'https://media.giphy.com/media/xUNd9RF49KWkiY3rhe/giphy.gif'
-godImage = 'https://drive.google.com/open?id=1BdbMGEtJEF4g9v1DW3TlEzhPAhIyquOY'
+godImage = 'https://imgur.com/a/sDV6p'
 siVocal = "https://drive.google.com/open?id=1UGuBto7q97m8qUWOiCH2bQYOoMrBlKgM"
 
 bot = telepot.Bot(token)
+
 
 def on_chat_message(msg):
     chat_id = msg['chat']['id']  # prelevo id del mittente
@@ -27,9 +29,12 @@ def on_chat_message(msg):
         bot.sendMessage(chat_id, ("Tara no!"))
     elif 'come stai' in msgText:
         bot.sendMessage(chat_id, ("Oggi cominci male!"))
-    elif 'sei pronto' in msgText:        bot.sendMessage(chat_id, ("Che ansia"))
+    elif 'sei pronto' in msgText:
+        bot.sendMessage(chat_id, ("Che ansia"))
     elif 'lr' in msgText:
         bot.sendDocument(chat_id, gifBestemmia)
+    elif 'dio' in msgText:
+        bot.sendPhoto(chat_id, godImage)
 
 
 bot.message_loop({'chat': on_chat_message}, run_forever=True)
