9436e89438b0e9bfbaf72167f9296117c0ac9b0f
Marco Gomiero <marco.gomiero.93@gmail.com>
2017-12-20 17:25:45 +0100

    code cleaning

--- bot.py
+++ bot.py
@@ -13,16 +13,18 @@ bot = telepot.Bot(token)
 
 
 def on_chat_message(msg):
-    chat_id = msg['chat']['id']  # prelevo id del mittente
-    msgText = msg['text']  # prelevo testo del messaggio
-    # username = msg['chat']['username'] #prelevo username del mittente
 
-    # Log su shell
-    print('Command Received ' + msgText)
+    # CHAT META
 
-    firstName = msg['from']['first_name']
-    lastName = msg['from']['last_name']
+    # chat id
+    chat_id = msg['chat']['id']
+    # message text lower cased for matching purpouse
+    msgText = msg['text']
     msgText = msgText.lower()
+    # Name
+    firstName = msg['from']['first_name']
+    # Surname
+    lastName = msg['from']['last_name']
 
     if 'ciao luca rossi' in msgText:
         bot.sendMessage(chat_id, ("Tara no!"))
